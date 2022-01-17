e3a893b109a227889e88e3b711bcf1e846181e1b
Jane Capants <78875579+janecapants@users.noreply.github.com>
2022-01-17 11:55:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,11 +21,16 @@ function App(){
     setTodos(newTodos);
     setValue('');
   }
-
+  const removeTodo = e => {
+    const index = Number(e.target.id);
+    let temp = [...todos];
+    temp.splice(index,1);
+    setTodos(temp);
+  }
   return(
     <>
       {todos.map((todo, i) => (
-        <div className="todo" key={i}>{todo.text}</div>
+        <div className="todo" key={i} id={i} onClick={removeTodo}>{todo.text}</div>
       ))}
       <form onSubmit={handleSubmit}>
         <input
